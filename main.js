3524b5185f749058127da7a4ce1d955f7291e7ba
Marlon Howard <marl04coder@outlook.com>
2020-09-16 22:44:24 -0400

    still working

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
+const numberButtons = document.querySelectorAll('[data-number')
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+       console.log('clicked')
+    })
+})
+
+const operationButtons = document.querySelectorAll('[data-operation]')
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+       console.log("clicked")
+    })
+})
+const equalsButtons = document.querySelector('[data-equals]')
+    equalsButtons.addEventListener('click', () => {
+       console.log("clicked")
+    })
+
+const deleteButtons = document.querySelector('[data-delete]')
+deleteButtons.addEventListener('click', () => {
+       console.log("clicked")
+    })
+
+const allClearButtons = document.querySelector('[data-all-clear]')
+allClearButtons.addEventListener('click', () => {
+       console.log("clicked")
+    })
 
 
-let calculator = document.querySelector("#calculator")
+
+/*let calculator = document.querySelector("#calculator")
 console.log(calculator)
 
 calculator.addEventListener("click", function(event){
     console.log("clicked")
-})
+})*/
